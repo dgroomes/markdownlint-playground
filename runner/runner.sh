59dc0f873c66cda74ae15760cfb8cb2f17580138
David Groomes <dwgroomes@gmail.com>
2021-08-09 19:25:08 -0500

    Handle symlinks in the runner shell script

--- runner/runner.sh
+++ runner/runner.sh
@@ -1,4 +1,13 @@
 #!/usr/bin/env bash
 # This file is the shell runner script to run the "runner" program! Phew! That's a lot of runners.
 
-node src/index.js "$@" | less
+# Get the script's containing directory, handling symlinks. See https://stackoverflow.com/a/246128
+SOURCE="${BASH_SOURCE[0]}"
+while [ -h "$SOURCE" ]; do # resolve $SOURCE until the file is no longer a symlink
+  DIR="$( cd -P "$( dirname "$SOURCE" )" >/dev/null 2>&1 && pwd )"
+  SOURCE="$(readlink "$SOURCE")"
+  [[ $SOURCE != /* ]] && SOURCE="$DIR/$SOURCE" # if $SOURCE was a relative symlink, we need to resolve it relative to the path where the symlink file was located
+done
+DIR="$( cd -P "$( dirname "$SOURCE" )" >/dev/null 2>&1 && pwd )"
+
+node "$DIR/src/index.js" "$@" | less
